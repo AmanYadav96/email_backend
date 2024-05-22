9c04e7b60fe642448abac93fc88622c85698d567
Keshav Agrawal <keshava.bca2022@ssism.org>
2024-05-22 15:56:15 +0530

    Message Sended

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -3,7 +3,6 @@ const { email } = require("../utils");
 const sendEmail = async (req, res) => {
   try {
     const response = await email.send(req.body);
-    console.log(req);
     if (response) {
       console.log(response);
     }
